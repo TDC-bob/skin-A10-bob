1c231fd441f8f961377780113cbfacd6060f94e1
bob <dariboucafix@daribouca.net>
2013-06-11 04:23:29 +0200

    removed numbers

--- SAVED_GAMES/Liveries/A-10C/TDC-Bob/description.lua
+++ SAVED_GAMES/Liveries/A-10C/TDC-Bob/description.lua
@@ -11,10 +11,10 @@ livery = {
 	{"A-10C_PAINT_1-j", 0 ,"A-10C_tdc-j",true};
 	{"A-10C_PAINT_1-k", 0 ,"A-10C_tdc-kbob",false};
 	{"A-10C_PAINT_1-L", 0 ,"A-10C_tdc-Lbob",false};
-	{"A-10_Number", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number", 0 ,"empty",true};
 	{"A-10_Number_Noze_F", 0 ,"empty",true};
-	{"A-10_Number_Noze_T", 0 ,"TactNumbers-USAF-Light_black",true};
-	{"A-10_Number_Wheel", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
 
 
 }
